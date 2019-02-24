09a3c88ae2e35849b63224d6c92351a5fb20518d
Marcelo Alexandre <cecel.alex@gmail.com>
2019-02-24 14:02:45 -0300

    Atualização final da versão 1.0

--- Views/template.php
+++ Views/template.php
@@ -24,16 +24,13 @@
   <div class="container">
   <header>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="<?php echo BASE_URL; ?>">Sandubão Itapecerica</a>
+    <a class="navbar-brand" href="<?php echo BASE_URL; ?>"><img src="<?php echo BASE_URL; ?>assets/images/logosandubaosite.png" /></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="<?php echo BASE_URL; ?>">Início</a>
-        </li>
         <li class="nav-item">
           <a class="nav-link" href="<?php echo BASE_URL; ?>dishes">Cardápio</a>
         </li>
@@ -53,11 +50,6 @@
 </header>
   <div class="container">
     <?php $this->loadViewInTemplate($viewName, $viewData); ?>
-  </div>
-
-  <div class="app-home">
-    <a href="" onclick="alert('App em desenvolvimento, em breve estará disponível na Play-store!');"><img src="<?php echo BASE_URL; ?>assets/images/icons/play-store.png" alt="Play-store" /></a>
-    <a href="" onclick="alert('App em desenvolvimento, em breve estará disponível na App-store!');"><img src="<?php echo BASE_URL; ?>assets/images/icons/app-store.png" alt="App-store" /></a>
   </div>
 
   <footer class="container">
